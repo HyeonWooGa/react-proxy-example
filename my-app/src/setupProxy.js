c3494792a4219e21a605721a6d1d183dccf79b70
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-13 18:18:42 +0900

    ♻️ http-proxy-middleware 셋업 리팩토링

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -2,31 +2,13 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api2/todos",
-    createProxyMiddleware({
-      target: "http://localhost:3070",
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/api2/todo",
-    createProxyMiddleware({
-      target: "http://localhost:3070",
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/api/books",
+    ["/api", "/api2"],
     createProxyMiddleware({
       target: "http://localhost:3080",
       changeOrigin: true,
-    })
-  );
-  app.use(
-    "/api/book",
-    createProxyMiddleware({
-      target: "http://localhost:3080",
-      changeOrigin: true,
+      router: {
+        "/api2": "http://localhost:3070",
+      },
     })
   );
 };
