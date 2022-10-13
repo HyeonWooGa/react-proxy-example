fb94af06aa36137a2ba44900e0d4dab92dab4b2d
HyeonWooGa <yeonwoopark22@gmail.com>
2022-10-13 14:30:16 +0900

    🎨 구조 변경 및 네비게이션 추가

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,63 +1,30 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import BookTable from './components/BookTable';
-import DisplayBoard from './components/DisplayBoard';
-import CreateBook from './components/CreateBook';
-import { getAllBooks, createBook } from './services/BookService';
-import Footer from './components/Footer';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Books from "./routes/Books";
+import Todos from "./routes/Todos";
 
-function App () {
-
-  const [bookShelf, setBookShelf] = useState({});
-  const [books, setBooks] = useState([]);
-  const [numberOfBooks, setNumberBooks] = useState(0);
-
-  const handleSubmit = () => {
-      createBook(bookShelf)
-        .then(() => {
-          setNumberBooks(numberOfBooks+1);
-      });
-  }
-
-  const getAllBook = () => {
-    getAllBooks()
-      .then(data => {
-        setBooks(data);
-        setNumberBooks(data.length);
-      });
-  }
-
-  const handleOnChangeForm = (e) => {
-      let inputData = bookShelf;
-      if (e.target.name === 'book') {
-        bookShelf.book = e.target.value;
-      } else if (e.target.name === 'category') {
-        bookShelf.category = e.target.value;
-      } else if (e.target.name === 'author') {
-        bookShelf.author = e.target.value;
-      }
-      setBookShelf(inputData);
-  }
-
-  
+function App() {
   return (
-    <div className="main-wrapper">
-      <div className="main">
-        <Header />
-        <CreateBook 
-          bookShelf={bookShelf}
-          onChangeForm={handleOnChangeForm}
-          handleSubmit={handleSubmit}
-        />
-        <DisplayBoard 
-          numberOfBooks={numberOfBooks} 
-          getAllBook={getAllBook} 
-        />
-        <BookTable books={books} />
-        <Footer />
-      </div>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <Link to="/books">
+            <li>Books</li>
+          </Link>
+          <Link to="todos">
+            <li>Todos</li>
+          </Link>
+        </ul>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/todos" element={<Todos />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
